5c90bd9e30da90c1273e13cf179a4e4650143cf7
Farooq Karimi Zadeh <farooghkarimizadeh@gmail.com>
2018-03-31 12:01:51 +0430

    updating copyright year

--- drawlogo86.asm
+++ drawlogo86.asm
@@ -1,5 +1,5 @@
 ;
-; Copyright (C) 2017 Farooq Karimi Zadeh <farooghkarimizadeh at gmail dot com>
+; Copyright (C) 2017-2018 Farooq Karimi Zadeh <farooghkarimizadeh at gmail dot com>
 ;
 ; Permission is hereby granted, free of charge, to any person obtaining a copy
 ; of this software and associated documentation files (the "Software"), to deal
